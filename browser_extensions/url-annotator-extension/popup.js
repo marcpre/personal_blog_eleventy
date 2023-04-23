9502f422afe9cb3dd6d89d962c2c07732d506c75
marcus <marcus.presich@gmail.com>
2023-04-23 08:39:33 +0200

    minor changes

--- browser_extensions/url-annotator-extension/popup.js
+++ browser_extensions/url-annotator-extension/popup.js
@@ -19,18 +19,49 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     });
+
+      // Merged code from background_worker.js
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "getAnnotatedUrls") {
+      chrome.storage.local.get("annotatedUrls", (data) => {
+        sendResponse({ annotatedUrls: data.annotatedUrls });
+      });
+      return true;
+    } else if (request.action === "saveAnnotatedUrl") {
+      chrome.storage.local.get("annotatedUrls", (data) => {
+        let annotatedUrls = data.annotatedUrls || [];
+        annotatedUrls.push(request.urlData);
+        chrome.storage.local.set({ annotatedUrls: annotatedUrls }, () => {
+          sendResponse({ success: true });
+        });
+      });
+      return true;
+    } else if (request.action === "removeAnnotatedUrl") {
+      chrome.storage.local.get("annotatedUrls", (data) => {
+        let annotatedUrls = data.annotatedUrls || [];
+        annotatedUrls = annotatedUrls.filter((urlData) => urlData.url !== request.url);
+        chrome.storage.local.set({ annotatedUrls: annotatedUrls }, () => {
+          sendResponse({ success: true });
+        });
+      });
+      return true;
+    }
+  });
   
-    function annotateUrl(url, description = "", number = "") {
-      const urlItem = document.createElement("div");
-      urlItem.className = "urlItem";
+  function annotateUrl(url, description = "", number = "") {
+    const urlItem = document.createElement("div");
+    urlItem.className = "urlItem";
   
       const urlSpan = document.createElement("span");
       urlSpan.textContent = url;
       urlItem.appendChild(urlSpan);
-  
+    
       const annotationInput = document.createElement("textarea");
       annotationInput.value = description;
       annotationInput.placeholder = "Add a longer description";
+      annotationInput.addEventListener("input", () => {
+        updateAnnotatedUrl(url, annotationInput.value, numberInput.value);
+      });
       urlItem.appendChild(annotationInput);
   
       const numberInput = document.createElement("input");
@@ -39,6 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
       numberInput.placeholder = "Add a positive or negative number";
       numberInput.addEventListener("input", () => {
         numberInput.style.color = numberInput.value >= 0 ? "green" : "red";
+        updateAnnotatedUrl(url, annotationInput.value, numberInput.value);
       });
       urlItem.appendChild(numberInput);
   
@@ -66,6 +98,18 @@ document.addEventListener("DOMContentLoaded", () => {
         let annotatedUrls = data.annotatedUrls || [];
         annotatedUrls = annotatedUrls.filter((urlData) => urlData.url !== url);
         chrome.storage.local.set({ annotatedUrls: annotatedUrls });
+      });
+    }
+
+    function updateAnnotatedUrl(url, description, number) {
+      chrome.storage.local.get("annotatedUrls", (data) => {
+        let annotatedUrls = data.annotatedUrls || [];
+        const index = annotatedUrls.findIndex((urlData) => urlData.url === url);
+        if (index !== -1) {
+          annotatedUrls[index].description = description;
+          annotatedUrls[index].number = number;
+          chrome.storage.local.set({ annotatedUrls: annotatedUrls });
+        }
       });
     }
   });
